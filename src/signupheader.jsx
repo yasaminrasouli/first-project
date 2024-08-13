b5d658935764cc478216b1411fe1c8731d8c0947
yasamin <yasaminrasoulii78@gmail.com>
2024-08-13 20:33:57 +0330

    Add initial version of the project

--- src/signupheader.jsx
+++ src/signupheader.jsx
@@ -5,7 +5,7 @@ function SignUpHeader() {
     <>
       <div className="bg-slate-100 h-10 border-b-2 border-red-700 sm:w-full shadow-slate-500/50"></div>
       <div
-        className="bg-header-logo bg-center bg-no-repeat text-center bg-white bg-right-center h-20 hidden md:block sm:hidden"
+        className="bg-header-logo bg-center bg-no-repeat text-center bg-white h-20"
         style={{
           boxShadow: "rgb(209, 209, 209) 0px 2px 5px",
         }}
